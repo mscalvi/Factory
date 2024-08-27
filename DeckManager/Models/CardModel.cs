87fd6bb27ab999b04ed04dcc230806b4fc095ec0
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-08-26 21:19:06 -0300

    Project Start

---
+++ DeckManager/Models/CardModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckManager.Models
+{
+    public class CardModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Owned { get; set; }
+
+
+        public string ManaCost { get; set; }
+        public string Type { get; set; }
+        public string Supertype { get; set; }
+        public string Text { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ScryfallId { get; set; }
+        public string OracleId { get; set; }
+
+
+        public string Edition { get; set; }
+        public string Condition { get; set; }
+        public string Extras { get; set; }
+
+    }
+}
---
+++ DeckManager/Services/ScryfallService.cs
@@ -0,0 +1,48 @@
+﻿using DeckManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DeckManager.Services
+{
+    public class ScryfallService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ScryfallService()
+        {
+            _httpClient = new HttpClient { BaseAddress = new Uri("https://api.scryfall.com/") };
+        }
+
+        public async Task<List<CardModel>> GetCardByName(string cardName)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"cards/named?fuzzy={cardName}");
+            response.EnsureSuccessStatusCode();
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            var cardData = JsonConvert.DeserializeObject<CardData>(responseContent);
+            return cardData?.Data ?? new List<CardModel>();
+        }
+
+        public async Task<CardModel> GetCardById(string scryfallId)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"cards/{scryfallId}");
+            response.EnsureSuccessStatusCode();
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CardModel>(responseContent);
+        }
+    }
+
+    public class CardData
+    {
+        public string Object { get; set; }
+        public List<CardModel> Data { get; set; }
+        public bool HasMore { get; set; }
+    }
+
+}
+
