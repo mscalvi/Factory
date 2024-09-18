67f44d7a718c58ab8099834c960ef4c76f98fec7
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-09-18 20:34:25 -0300

    Not much

--- BingoManager/obj/Debug/net8.0-windows/BingoManager.AssemblyInfo.cs
+++ BingoManager/obj/Debug/net8.0-windows/BingoManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BingoManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a7fcb7ea20a75a14a59933d42bd9a6c32018cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bba818766cc833b74ae231854ea69fbbc1fb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BingoManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BingoManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
